e8eea398ba8b82f2b8cd84eaf08c142c8af51f0e
abulbashar98 <abulbashar98116@gmail.com>
2022-01-13 02:45:08 +0600

    dynamic-api

--- js/countries.js
+++ js/countries.js
@@ -8,20 +8,48 @@ const loadCountries = () => {
 loadCountries()
 
 const displayCountries = countries => {
-    console.log(countries)
+
     const countriesSection = document.getElementById('countries')
     for (const country of countries) {
         countries.forEach(country => {
             const div = document.createElement('div');
             // div.classList.add(country)       // Made this silly silly Mistake
             div.classList.add('country')
-            const h3 = document.createElement('h3')
-            h3.innerText = country.name.common
-            div.appendChild(h3);
-            const p = document.createElement('p')
-            p.innerText = country.capital
-            div.appendChild(p)
-            countriesSection.appendChild(div)
+            // console.log(country.name.common, country.name.official)
+            div.innerHTML = `
+                              <h3>${country.name.common}</h3>
+                              <p>${country.capital}</p>
+                              <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+            `
+            countriesSection.appendChild(div);
+
+            // console.log(country)
+            // const h3 = document.createElement('h3')
+            // h3.innerText = country.name.common
+            // div.appendChild(h3);
+            // const p = document.createElement('p')
+            // p.innerText = country.capital
+            // div.appendChild(p)
+            // countriesSection.appendChild(div)
         });
     }
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+
+const displayCountryDetails = country => {
+    console.log(country)
+
+    const countryDetailContainer = document.getElementById('country-detail')
+
+    countryDetailContainer.innerHTML = `
+        <h5>Name: ${country.name.common}</h5>
+    <p>Population: ${country.population} </p>
+    <img width="200px" src="${country.flags.png}" alt="">
+    `
 }
